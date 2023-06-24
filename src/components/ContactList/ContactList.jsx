0ae87cd2623f3ade29a9f977b9af2e74816a837f
Nata-Curly <nkucheryava@gmail.com>
2023-06-24 11:32:12 +0200

    first commit

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,13 +1,14 @@
-import { List, ListItem, DeleteBtn } from './ContactList.styled';
+import ContactItem from 'components/ContactItem/ContactItem';
+import { List } from './ContactList.styled';
 import PropTypes from 'prop-types';
+
 
 const ContactList = ({contacts, onDeleteContact}) => (
     <List>
         {contacts.map(({ id, name, number }) => (
-            <ListItem key={id}> 
-                <p>{name}: {number}</p>
-            <DeleteBtn type="button" onClick={()=>onDeleteContact(id, name)}>Delete</DeleteBtn>
-            </ListItem>
+            <li key={id}> 
+                <ContactItem name={name} number={number} onDelete={() => onDeleteContact(id, name)} />
+            </li>
         ))}
     </List>
 )
