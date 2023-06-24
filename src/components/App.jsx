743021c95078086ddd40039be02ebd6c58c822ce
Nata-Curly <nkucheryava@gmail.com>
2023-06-24 12:59:28 +0200

    local storage HW

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,7 +9,7 @@ import { showInfoMessage, showErrorMessage } from './Notification';
 
 class App extends Component {
   state = {
-    contacts: initialContacts, 
+    contacts: initialContacts,
     filter: '',
   }
 
@@ -27,7 +27,7 @@ class App extends Component {
       contacts: [contact, ...contacts]
     })) : showInfoMessage(
       `The contact with name "${data.name}" is already in your phonebook`
-    ); 
+    );
   };
 
   changeFilter = (evt) => {
@@ -38,7 +38,7 @@ class App extends Component {
     const { contacts, filter } = this.state;
     const normalizedFilter = filter.toLowerCase();
     return contacts.filter(contact => contact.name.toLowerCase().includes(normalizedFilter));
-   }
+  }
 
   deleteContact = (contactId, name) => {
     this.setState(prevState => ({
@@ -47,7 +47,27 @@ class App extends Component {
     showErrorMessage(`You have deleted a contact "${name}"`);
   };
 
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    };
+    
+  };
+  componentDidUpdate(PrevProps, PrevState)
+  {
+    // console.log('App componentDidUpdate');
+    // console.log(PrevState);
+    // console.log(this.state);
+    if (this.state.contacts !== PrevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  };
+
   render() {
+    // console.log('App render');
+
     const { filter } = this.state;
     
     const visibleContacts = this.getVisibleContacts();
